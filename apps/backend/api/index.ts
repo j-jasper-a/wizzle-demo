b93c664aff048ba4366fbe274e44a7c28c8e8148
Jihan Jasper Al Rashid <johndoe@example.com>
2025-03-05 16:18:10 +0600

    feat(backend): integrate Firebase service account

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,29 +1,21 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-
-/*
 import admin from "firebase-admin";
-import dotenv from "dotenv";
-
-
-dotenv.config({ path: `.env.local` });
 
 const serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
 export const db = admin.firestore();
 export const auth = admin.auth();
-
-*/
 
 const app = express();
 
